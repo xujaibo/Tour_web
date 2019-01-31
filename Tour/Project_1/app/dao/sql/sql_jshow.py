50e9c5485e357b1ffeb2ca991427ef12a927b2ca
xujaibo <321627903@qq.com>
2019-01-31 11:30:07 +0800

    new html
    11.30

--- Tour/Project_1/app/dao/sql/sql_jshow.py
+++
@@ -1,7 +0,0 @@
-
-
-
-sql_jshow={
-    "get_suzhou":"select introduction,img,have_go from jshow where citys='{citys}' limit 1",
-
-}
--- Tour/Project_1/app/dao/sql/sql_user.py
+++
@@ -1,8 +0,0 @@
-
-
-
-sql_user={
-    "addUser":"insert into user(telephone,password,regist_date) values('{telephone}','{password}',CURDATE())",
-    "getUserById":"select id,telephone,email,regist_date from user where telephone={telephone} limit 1",
-    "landing_by_user":"select id from user where telephone={telephone} and password={password} limit 1"
-}
